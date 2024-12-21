1fa7a383fc9d368f458dd240edceba77d2451961
Ricky Yang <ricky.yang@ef.com>
2024-12-21 14:43:17 +0800

    add TestServlet

---
+++ webroot/TestServlet.java
@@ -0,0 +1,9 @@
+ 
+    @Override
+    protected void doGet(com.microtomcat.connector.Request request, 
+                        com.microtomcat.connector.Response response) 
+            throws com.microtomcat.servlet.ServletException, java.io.IOException {
+        String content = "<html><body><h1>Hello from TestServlet in webroot!</h1></body></html>";
+        response.sendServletResponse(content);
+    }
+}
