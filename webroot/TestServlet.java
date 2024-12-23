e5e03ee4756c475427dea2940e01fa1c919e24d7
Ricky Yang <ricky.yang@ef.com>
2024-12-23 21:01:27 +0800

    update

--- webroot/TestServlet.java
+++
@@ -1,9 +0,0 @@
-public class TestServlet extends com.microtomcat.servlet.HttpServlet {
-    @Override
-    protected void doGet(com.microtomcat.connector.Request request, 
-                        com.microtomcat.connector.Response response) 
-            throws com.microtomcat.servlet.ServletException, java.io.IOException {
-        String content = "<html><body><h1>Hello from TestServlet in webroot!</h1></body></html>";
-        response.sendServletResponse(content);
-    }
-}
